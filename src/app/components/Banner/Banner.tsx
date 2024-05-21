33727d73088cfa6d703f91ff6da825b1162892ef
gomestzx <felipematheusdev@gmail.com>
2024-05-21 15:47:46 -0300

    refactor: change banner

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <>
             <div className='w-full md:w-4/6 mx-auto mt-8 flex justify-center flex-wrap rounded-md' id='perguntas-frequentes'>
                 <Link href='/placa-pix'>
-                    <Image className=' rounded-md' src='/banner.png?v=' height={167} width={800} alt='' />
+                    <Image className=' rounded-md' src='/banner.png?v=3' height={167} width={800} alt='' />
                 </Link>
             </div >
         </>
