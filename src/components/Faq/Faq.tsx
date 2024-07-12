1121bc6ad7a71df374f5c951a00c86c71281686d
gomestzx <felipematheusdev@gmail.com>
2024-07-12 03:37:30 -0300

    refactor: faq content

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -9,23 +9,16 @@ const Faq = () => {
             >
 
                 <Accordion
-                    title='Como criar minha placa personalizada?'
-                    content={
-                        <div> <p className='mb-2'>Basta adicionar os dados obrigatórios: </p>
-                            <ul>
-                                <li className='text-teal-700 text-bold'>1 - Chave PIX</li>
-                                <li className='text-teal-700'>2 - Nome</li>
-                                <li className='text-teal-700'>3 - Cidade</li>
-                            </ul>
-                            <p className='mt-2'> e clicar no botão "Criar Placa Pix" </p></div>}
+                    title='Como gerar QR Code para minha chave PIX?'
+                    content='Preencha o formulário com os dados necessários, como chave Pix, valor da transação (se desejar um QR Code estático), e qualquer outra informação relevante. Após preencher, clique no botão "Baixar QR Code"'
                 />
                 <Accordion
-                    title='Como mudar o design?'
-                    content='Disponiblizamos vários tipos de design para que possam atender a sua demanda. Para escolher, basta clicar no design desejado.'
+                    title='Como gerar placas personalizadas para minha chave PIX?'
+                    content='Clique no botão "Gerar Placa Pix", preencha com os dados necessários, escolha o design de sua preferência e faça o download da placa.'
                 />
                 <Accordion
                     title='O gerador de placa personalizada é gratuito?'
-                    content='Todas as ferramentas do nosso site são 100% gratuitas'
+                    content="Sim, todas as ferramentas disponíveis em nosso site são completamente gratuitas."
                 />
 
             </div></>
